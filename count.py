b4b56892a4552c64ede8a482b117c65c9b9b1ea3
Jaemin Jo <jmjo@hcil.snu.ac.kr>
2017-04-04 16:55:45 +0900

    added force-directed layout

--- count.py
+++ count.py
@@ -4,7 +4,28 @@ import csv
 from collections import defaultdict
 import operator
 import pprint
+import json
+import itertools
 
+def make_graph(papers):
+  authors_count = defaultdict(int)
+  rel_count = defaultdict(lambda: defaultdict(int))
+
+  for paper in papers:
+    for author in paper['authors']:
+      authors_count[author] += 1
+
+    for pair in itertools.combinations(paper['authors'], 2):
+      rel_count[pair[0]][pair[1]] += 1
+
+  result = {}
+  result['nodes'] = [{'id': name, 'count': count} for name, count in authors_count.items()]
+  result['links'] = [{'source': name1, 'target': name2, 'count': count} for name1, d in rel_count.items() for name2, count in d.items()]
+
+  with open('graph.json', 'w') as out:
+    json.dump(result, out)
+  #    'links': [{'source': }]
+  #    }
 
 def main():
   authors_count = defaultdict(int)
@@ -49,6 +70,8 @@ def main():
   pp.pprint(sorted(authors_count.items(), key=operator.itemgetter(1), reverse=True)[:10])
   pp.pprint(sorted(countries_count.items(), key=operator.itemgetter(1), reverse=True)[:10])
   pp.pprint(sorted(keywords_count.items(), key=operator.itemgetter(1), reverse=True)[:10])
+
+  make_graph(papers)
 
 
 if __name__ == "__main__":
